9cd044b0eec207d786ba56d64b1b12e41db38334
Ewapotranspiracja <32907073+Ewapotranspiracja@users.noreply.github.com>
2019-11-24 14:07:22 +0100

    checking if username exists before registration

--- public/web/scripts/main.js
+++ public/web/scripts/main.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 
     var username_free = false;
-    
+
     //////////////////function for end typing
     (function($) {
 $.fn.donetyping = function(callback){
     var _this = $(this);
-    var x_timer;    
+    var x_timer;
     _this.keyup(function (){
         clearTimeout(x_timer);
         x_timer = setTimeout(clear_timer, 1000);
-    }); 
+    });
 
     function clear_timer(){
         clearTimeout(x_timer);
@@ -20,80 +20,76 @@ $.fn.donetyping = function(callback){
 })(jQuery);
     $('#tabReg').hide();
 
-/*
+
     //////Function for checking if username is available
-  $('#username').donetyping(function(callback)           
+  $('.userConfirm').donetyping(function(callback)
                        {
   ///$('#username').css('border','none');
-  var username = $('#username').val()
-       
-    
+  var username = $('.userConfirm').val()
+
+    console.log("elo " + username);
     if(username!=''){
-        
-         
-        
-        
+
+
+
+
         var request = new XMLHttpRequest()
         var exists = 0;
-        
-request.open('GET', 'http://127.0.0.1:8000/user_exists/'+username, true)
+
+request.open('GET', 'http://127.0.0.1:8000/userExists/'+username, true)
 request.onload = function() {
-    
+
   var data = JSON.parse(this.response)
 
   if (request.status >= 200 && request.status < 400) {
-  
-      if (data.user_exists == true){
+  console.log(data);
+      if (data[0] == true){
             $('#status').css('color','darkred');
               $('#status').html('<b>Login jest już zajęty!</b>');
          username_free = false;
       }
       else {
-          
-            $('#status').css('color','black');
-              $('#status').html('<b>Login dostępny!</b>');
+          $('#status').html('<b> </b>');
+          $('#usernameicon').css('background-color','#7DA2AA');
           username_free = true;
       }
-      
+
   } else {
     console.log('error')
   }
 }
 
 request.send()
-        
-        
+
+
     }
     else {
-         $('#status').html('<b>Nazwa użytkownika</b>');
+         $('#status').html('<b> </b>');
     $('#status').css('color','black');
     }
-    
+
     });
 
 
-  */
-    ////////function for submiting form
-    
-    $('#register-form').click(function() {
-        if(username_free){ //if form is valid submit form
-			return true;
-		}
-         $('#username').css('border','1.5px solid red');
-		event.preventDefault();
-        
-    }
-           
-        
-        
-    
-                              );
-    
-    ///////////
+
+    $('#registerForm').on("submit",function()
+    {
+
+        if (username_free) {
+            return true;
+        } else {
+            $('#usernameicon').css('background-color','darkred');
+            return false;
+        }
+
+
+    });
+
+
+
 
     $("#prevBtn").on("click", function () {
         event.preventDefault();
-        console.log("saa");
         $('#tabLogin').slideUp();
         $('#tabReg').slideDown();
     });
